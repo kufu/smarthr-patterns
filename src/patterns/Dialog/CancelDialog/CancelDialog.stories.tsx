02f352e4a685faee75104597d85b8ce513fd0811
yamish123 <32166731+yamish123@users.noreply.github.com>
2022-08-16 16:19:58 +0900

    レビュー指摘修正

--- src/patterns/Dialog/CancelDialog/CancelDialog.stories.tsx
+++ src/patterns/Dialog/CancelDialog/CancelDialog.stories.tsx
@@ -4,45 +4,42 @@ import { ActionDialog, Text } from 'smarthr-ui'
 import styled, { css } from 'styled-components'
 
 const Wrapper = styled.div`
-  ${({ theme: { spacing } }) => css`
-    padding: ${spacing.M};
+  ${({ theme: { space } }) => css`
+    padding: ${space(1.5)};
   `}
 `
 
-const Template: Story<StoryProps> = () => {
+const Template: Story = () => {
   const [isOpen, setIsOpen] = useState(true)
   return (
-    <>
-      <ActionDialog
-        isOpen={isOpen}
-        title="{操作名}を取り消しますか？"
-        actionText="取り消し"
-        actionTheme="danger"
-        onClickAction={() => ({})}
-        onClickClose={() => setIsOpen(false)}
-        onClickOverlay={() => setIsOpen(false)}
-      >
-        <Wrapper>
-          <Text as="p">
-            {'{操作名}'}を取り消します。
-            <br />
-            「取り消し」を押すと変更内容が破棄されます。
-          </Text>
-        </Wrapper>
-      </ActionDialog>
-    </>
+    <ActionDialog
+      isOpen={isOpen}
+      title="{操作名}を取り消しますか？"
+      actionText="取り消し"
+      actionTheme="danger"
+      onClickAction={() => ({})}
+      onClickClose={() => setIsOpen(false)}
+      onClickOverlay={() => setIsOpen(false)}
+    >
+      <Wrapper>
+        <Text as="p">
+          {'{操作名}'}を取り消します。
+          <br />
+          「取り消し」を押すと変更内容が破棄されます。
+        </Text>
+      </Wrapper>
+    </ActionDialog>
   )
 }
 
 export const Default = Template.bind({})
-Default.args = {}
 Default.storyName = '基本'
 
 export default {
   title: 'ダイアログ/取り消しダイアログ',
   parameters: {
     docs: {
-      // Opt-out of inline rendering
+      // false にしないと、docs全体にダイアログがかかってしまう
       inlineStories: false,
     },
   },
--- src/patterns/Dialog/DeleteDialog/DeleteDialog.stories.tsx
+++ src/patterns/Dialog/DeleteDialog/DeleteDialog.stories.tsx
@@ -4,8 +4,8 @@ import { ActionDialog, Stack, Text } from 'smarthr-ui'
 import styled, { css } from 'styled-components'
 
 const Wrapper = styled(Stack)`
-  ${({ theme: { spacing } }) => css`
-    padding: ${spacing.M};
+  ${({ theme: { space } }) => css`
+    padding: ${space(1.5)};
   `}
 `
 
@@ -16,39 +16,36 @@ type StoryProps = {
 const Template: Story<StoryProps> = ({ hasEffect }) => {
   const [isOpen, setIsOpen] = useState(true)
   return (
-    <>
-      <ActionDialog
-        isOpen={isOpen}
-        title="{オブジェクト名}を削除しますか？"
-        actionText="削除"
-        actionTheme="danger"
-        onClickAction={() => ({})}
-        onClickClose={() => setIsOpen(false)}
-        onClickOverlay={() => setIsOpen(false)}
-      >
-        <Wrapper>
-          <Text as="p">
-            【{'{オブジェクトのインスタンス名}'}】を削除します。
-            {hasEffect && (
-              <>
-                <br />
-                {'{関連するオブジェクト名など}'}に登録されているデータも削除されます。
-                <br />
-                {'{選択したオブジェクトのインスタンス名}'}に含まれる{'{オブジェクト名}'}も削除されます。
-                <br />
-                削除した{'{オブジェクト名}'}は元に戻せません。
-              </>
-            )}
-          </Text>
-          {hasEffect && <Text as="p">{'{オブジェクト名}'}を削除しますか？</Text>}
-        </Wrapper>
-      </ActionDialog>
-    </>
+    <ActionDialog
+      isOpen={isOpen}
+      title="{オブジェクト名}を削除しますか？"
+      actionText="削除"
+      actionTheme="danger"
+      onClickAction={() => ({})}
+      onClickClose={() => setIsOpen(false)}
+      onClickOverlay={() => setIsOpen(false)}
+    >
+      <Wrapper>
+        <Text as="p">
+          【{'{オブジェクトのインスタンス名}'}】を削除します。
+          {hasEffect && (
+            <>
+              <br />
+              {'{関連するオブジェクト名など}'}に登録されているデータも削除されます。
+              <br />
+              {'{選択したオブジェクトのインスタンス名}'}に含まれる{'{オブジェクト名}'}も削除されます。
+              <br />
+              削除した{'{オブジェクト名}'}は元に戻せません。
+            </>
+          )}
+        </Text>
+        {hasEffect && <Text as="p">{'{オブジェクト名}'}を削除しますか？</Text>}
+      </Wrapper>
+    </ActionDialog>
   )
 }
 
 export const Default = Template.bind({})
-Default.args = {}
 Default.storyName = '基本'
 
 export const HasEffect = Template.bind({})
@@ -59,7 +56,7 @@ export default {
   title: 'ダイアログ/削除ダイアログ',
   parameters: {
     docs: {
-      // Opt-out of inline rendering
+      // false にしないと、docs全体にダイアログがかかってしまう
       inlineStories: false,
     },
   },
