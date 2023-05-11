f81dc75443c5646e7ad1f4d9051c10f6ce3073e0
yamish123 <32166731+yamish123@users.noreply.github.com>
2023-05-11 11:55:57 +0900

    new-frameworks migration

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,31 +1,20 @@
 const tsconfigPaths = require('vite-tsconfig-paths')
-
 module.exports = {
-  "stories": [
-    "../patterns/**/*.stories.mdx",
-    "../patterns/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  stories: ['../patterns/**/*.stories.mdx', '../patterns/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
-  "features": {
-    "storyStoreV7": true
+  features: {
+    storyStoreV7: true,
   },
   /* https://github.com/storybookjs/builder-vite/issues/85
    * alias を storybook with vite で効かせるため */
   viteFinal: async (config) => {
     return {
       ...config,
-      plugins: [
-        ...config.plugins,
-        tsconfigPaths.default(),
-      ]
+      plugins: [...config.plugins, tsconfigPaths.default()],
     }
-  }
+  },
 }
